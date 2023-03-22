6909c8350dd382bb543ca8171aa2ee190f708e48
BogachevaKA1 <BogachevaKA1@it.mos.ru>
2023-03-22 12:40:03 +0300

    exercise18

---
+++ exercise18/Program.cs
@@ -0,0 +1,51 @@
+﻿/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка */
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 10);      
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3} \t");
+        Console.WriteLine();
+    }
+}
+
+void SumArray(int[,] array)
+{
+    int minsum = Int32.MaxValue;
+    int z = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (sum < minsum)
+        {
+            minsum = sum;
+            z++;
+        }
+    }
+    Console.WriteLine($"Номер строки c наименьшей суммой элементов: {z}, сумма которой равна: {minsum}");
+}
+
+Console.WriteLine("Введите размер прямоугольного массива");
+int num = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[num, num];
+FillArray(array);
+PrintArray(array);
+SumArray(array);
